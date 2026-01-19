4a86eca5e85dc9c023eb529f749d35d65520bc71
wozniak2 <wozniac@gmail.com>
2026-01-19 09:07:32 +0100

    Update 7_hexagonizepop.py

--- scripts/7_hexagonizepop.py
+++ scripts/7_hexagonizepop.py
@@ -9,6 +9,10 @@ import numpy as np
 import rioxarray
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import h3pandas
+import pandas as pd
+
+
 fp = r"C:\\Users\\wozni\\Google Drive\\UAM\\HUB\\MobilityPatterns\\Data\\ghs_pop.tif"
 # create network from that bounding box
 o_bb = 17.402, 52.680, 16.460, 52.151
@@ -20,7 +24,7 @@ nodes, streets = ox.graph_to_gdfs(G)
 
 data = rioxarray.open_rasterio(fp, mask_and_scale=False)
 ap = gpd.read_file('ap.gpkg') ## poznan agglomeration without the city
-
+poz = gpd.read_file('poz.gpkg') 
 
 
 # convert raster to polygons
@@ -57,6 +61,19 @@ pop_grid = gpd.overlay(ghs_gdf, ap, how='intersection', keep_geom_type = False)
 ap_union = ap.geometry.unary_union
 ap_union = gpd.GeoSeries([ap_union], crs=pop_grid.crs)
 streets = streets.to_crs(crs=pop_grid.crs)
+
+
+# Resample to H3 cells
+ap_rep = ap_union.to_crs(crs='EPSG:4326')
+
+resolution=7
+# project
+ap = ap.to_crs(crs='EPSG:4326')
+hex_h3 = ap_rep.h3.polyfill_resample(resolution)
+hex_h3 = hex_h3.reset_index(drop=True)
+hex_h3["hex_id"] = hex_h3.index
+hex_h3.plot()
+
 # initialize plot
 import contextily as ctx
 
