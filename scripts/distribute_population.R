e675c45af4809c0b0fa148e83e742018d77db244
wozniak2 <wozniac@gmail.com>
2026-03-04 09:42:34 +0100

    Update distribute_population.R

--- scripts/distribute_population.R
+++ scripts/distribute_population.R
@@ -15,8 +15,43 @@ pop_geocoded <- st_as_sf(
   crs = 4326
 )
 
-## plot
+
+
+
+# Get the bounding box for ap
+bbox <- st_bbox(ap)
+
+
+# Create a polygon grid that covers the extent of the points
+# n argument defines the dimensions of the grid
+
+grid_poly <- st_make_grid(bbox, n=c(100,100), what = "polygons") %>%
+  st_as_sf() %>%
+  # Assign a unique ID to each grid cell
+  mutate(grid_id = row_number())
+
+grid_poly <- st_transform(grid_poly, crs = st_crs(pop_geocoded))
+
+# Spatially join the points to the grid polygons
+# st_join adds polygon attributes to the points data frame for points falling within each polygon
+points_joined <- st_join(pop_geocoded, grid_poly, join = st_within)
+
+# Aggregate the 'value' variable by 'grid_id' using dplyr
+# Sum the 'value' for all points within each grid cell
+aggregated_data <- points_joined %>%
+#  filter(!is.na(grid_id)) %>% # Remove points outside the main grid extent (if any)
+  group_by(grid_id) %>%
+  summarise(total_value = sum(WIEK_26_DO_60, na.rm = TRUE), 
+            point_count = n())
+
+aggregated_data <- st_drop_geometry(aggregated_data)
+
+# merge the results
+final_grid <- left_join(grid_poly, aggregated_data, by = "grid_id")
+
+## plot population
 ggplot()+
-  geom_sf(data = pop_geocoded, aes(color = WIEK_26_DO_60), size = 0.3, alpha = 0.2) +
-  geom_sf(data = ap, fill = NA) +
-  scale_color_viridis_c(n.breaks = 4, begin = 0.2, end = 1)
+#  geom_sf(data = pop_geocoded, aes(color = WIEK_26_DO_60), size = 0.3, alpha = 0.2) +
+  geom_sf(data = final_grid, aes(fill = total_value), color="lightgrey", lwd = 0.1, alpha = 0.7) +
+  geom_sf(data = ap, fill = NA, lwd = 0.5) +
+  scale_fill_viridis_c(begin = 0.1, end = 1)
