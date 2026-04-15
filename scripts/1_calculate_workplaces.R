006f26665979697b99f3c8387df8dac83fcc8ede
wozniak2 <wozniac@gmail.com>
2026-04-15 10:04:41 +0200

    upload plot_itineraries.R

--- scripts/1_calculate_workplaces.R
+++ scripts/1_calculate_workplaces.R
@@ -1,25 +1,29 @@
 library(dplyr)
+library(exactextractr)
 library(osmdata)
 library(sf)
 library(terra)
 library(tmap)
 
-setwd("/home/adam/Dokumenty/commuting_patterns")
+setwd("/Users/wozni/Google Drive/UAM/HUB/MobilityPatterns/Data")
 
 #Set global parameters
 tmap_mode("view")
 total_workplaces <- 371010 #June 2025
-ghsl_share <- 0.8
 sme_share <- 0.2
 vo_threshold <- 51
 
-#Get city boundary from OSM
-boundary <- opq('poznan poland') %>% 
-  add_osm_feature('key' = 'admin_level', 'value' = '7') %>% 
-  osmdata_sf() %>% 
-  unname_osmdata_sf()
-boundary <- boundary$osm_multipolygons %>% filter(name == "Poznań")
-st_write(boundary, "boundary.gpkg")
+#Load city boundary or get it from OSM
+if (file.exists("boundary.gpkg")) {
+  boundary <- st_read("boundary.gpkg")
+  } else {
+    boundary <- opq('poznan poland') %>% 
+    add_osm_feature('key' = 'admin_level', 'value' = '7') %>% 
+    osmdata_sf() %>% 
+    unname_osmdata_sf()
+    boundary <- boundary$osm_multipolygons %>% filter(name == "Poznań")
+    st_write(boundary, "boundary.gpkg")
+  }
 #OPTIONAL: Check if downloaded correctly
 #tm_shape(boundary) + tm_polygons()
 
@@ -27,11 +31,9 @@ st_write(boundary, "boundary.gpkg")
 ghsl_nonres <- rast("GHS_BUILT_V_NRES_E2025_GLOBE_R2023A_54009_100_V1_0_R3_C20.tif")
 boundary <- st_transform(boundary, st_crs(ghsl_nonres))
 
-#Crop to boundary extent and vectorize
+#Make 100x100m workplaces grid by vectorizing GHSL
 ghsl_nonres <- crop(ghsl_nonres, boundary)
 ghsl_nonres <- as.polygons(ghsl_nonres, aggregate = F)  %>% st_as_sf()
-
-#Make workplaces grid
 workplace_grid <- st_intersection(ghsl_nonres, boundary)
 workplace_grid <- rename(workplace_grid, nonres_volume = GHS_BUILT_V_NRES_E2025_GLOBE_R2023A_54009_100_V1_0_R3_C20) %>% 
   subset(select = "nonres_volume") #%>% filter(nonres_volume > 0) #Make human-readable & clean
@@ -40,6 +42,57 @@ workplace_grid <- workplace_grid %>% mutate(grid_id = row_number())
 workplace_grid <- st_transform(workplace_grid, st_crs <- 4326)
 workplace_grid <- st_make_valid(workplace_grid)
 
+#OR
+#Make 200x200m workplaces grid by aggregating GHSL
+bbox <- st_bbox(boundary)
+bbox <- st_transform(bbox, crs = 2180)
+workplace_grid <- st_make_grid(bbox, c(200, 200), what = "polygons") %>%
+  st_as_sf() %>%
+  # Assign a unique ID to each grid cell
+  mutate(grid_id = row_number())
+workplace_grid<- st_transform(workplace_grid, crs = st_crs(ghsl_nonres))
+workplace_grid$nonres_volume <- exact_extract(ghsl_nonres, workplace_grid, "sum")
+workplace_grid <- st_intersection(workplace_grid, boundary)
+workplace_grid$nonres_weight <- workplace_grid$nonres_volume / sum(workplace_grid$nonres_volume)
+workplace_grid <- subset(select = c("grid_id", "nonres_volume", "nonres_weight", "sme_count", "sme_weight"))
+workplace_grid <- st_transform(workplace_grid, crs = 4236)
+
+#OR
+#Make 200x200m grid and aggregate BDOT10k buildings data
+bbox <- st_bbox(boundary)
+bbox <- st_transform(bbox, crs = 2180)
+workplace_grid <- st_make_grid(bbox, c(200, 200), what = "polygons") %>%
+  st_as_sf() %>%
+  # Assign a unique ID to each grid cell
+  mutate(grid_id = row_number())
+buildings <- st_read("./work_bdot.gpkg")
+buildings <- buildings %>% filter(!FUNKCJAOGOLNABUDYNKU %in% c("budynki mieszkalne",
+                                                          "budynki produkcyjne, usługowe i gospodarcze dla rolnictwa",
+                                                          "budynki transportu i łączności"))
+buildings$total_floor_area <- as.numeric(st_area(buildings) * buildings$LICZBAKONDYGNACJI)
+buildings <- st_centroid(buildings)
+
+# Spatially join the points to the grid polygons
+# st_join adds polygon attributes to the points data frame for points falling within each polygon
+buildings_joined <- st_join(buildings, workplace_grid, join = st_within)
+
+# Aggregate the 'value' variable by 'grid_id' using dplyr
+# Sum the 'value' for all points within each grid cell
+aggregated_data <- buildings_joined %>%
+  #  filter(!is.na(grid_id)) %>% # Remove points outside the main grid extent (if any)
+  group_by(grid_id) %>%
+  summarise(bdot_area = sum(total_floor_area, na.rm = TRUE), 
+            point_count = n())
+aggregated_data <- st_drop_geometry(aggregated_data)
+
+# merge the results & clip to boundary
+workplace_grid <- left_join(workplace_grid, aggregated_data, by = "grid_id")
+boundary <- st_transform(boundary, crs = st_crs(workplace_grid))
+workplace_grid <- st_intersection(workplace_grid, boundary)
+workplace_grid <-  workplace_grid %>% subset(select = c("grid_id", "bdot_area", "point_count"))
+workplace_grid[is.na(workplace_grid)] <- 0
+workplace_grid$bdot_weight <- workplace_grid$bdot_area / sum(workplace_grid$bdot_area)
+workplace_grid <- st_make_valid(workplace_grid)
 #OPTIONAL: Plot to check
 #tm_shape(filter(workplace_grid, nonres_volume >0)) + tm_polygons(fill = "nonres_volume", col_alpha = 0, palette = "Reds")
 
@@ -48,6 +101,7 @@ sme <- read.csv("ceidg_geocode.csv")
 sme <- sme %>% filter(!is.na(g_dlug))
 sme <- st_as_sf(sme, coords = c("g_dlug", "g_szer"), crs = 4326)
 sme <- st_make_valid(sme)
+sme <- st_transform(sme, crs = st_crs(workplace_grid))
 
 #OPTIONAL: plot to check
 #tm_shape(sme) + tm_symbols()
@@ -71,7 +125,12 @@ workplace_grid$workplaces <- ((workplace_grid$nonres_weight * ghsl_share) +
                               (workplace_grid$sme_weight * sme_share)) * 
                                 total_workplaces
 
+#Calculate workplaces per grid cell - WITH BDOT
+workplace_grid$workplaces <- ((workplace_grid$bdot_weight * (1-sme_share)) +
+                                (workplace_grid$sme_weight * sme_share)) * 
+  total_workplaces
+
 st_write(workplace_grid, "workplace_grid.gpkg", append = FALSE)
 
 #OPTIONAL: Plot to check
-#tm_shape(filter(workplace_grid, workplaces >10)) + tm_polygons(fill = "workplaces", col_alpha = 0, fill_alpha = 0.5, palette = "Reds")
+tm_shape(filter(workplace_grid, workplaces >10)) + tm_polygons(fill = "workplaces", col_alpha = 0, fill_alpha = 0.5, palette = "Reds")
